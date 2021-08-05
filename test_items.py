2dd79907fd1f71f984c62d9cefc077a86723fa2e
Матиев Роман Владимирович <rvmatiyev@stream.local>
2021-08-05 16:48:44 +0500

    corrected_lesson_3.6

--- test_items.py
+++ test_items.py
@@ -4,8 +4,9 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_contain_button(browser):
     browser.get(link)
+    time.sleep(30)
     button = browser.find_element_by_css_selector("form#add_to_basket_form>button")
-    time.sleep(10)
+
 
 
     assert button, "Кнопка не найдена"
